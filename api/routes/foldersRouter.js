43f58d317206623430ba72a81a01c7e7ea5687d7
Ashley Lin <linashleyy@gmail.com>
2025-06-05 07:14:09 +0800

    Get folder with a specific id

--- api/routes/foldersRouter.js
+++ api/routes/foldersRouter.js
@@ -1,5 +1,5 @@
-import { getFolders } from "#db/queries/folders";
 import express from "express";
+import { getFolderById, getFolders } from "#db/queries/folders";
 
 const foldersRouter = express.Router();
 
@@ -7,6 +7,24 @@ foldersRouter.get("/", async (req, res, next) => {
   try {
     const folders = await getFolders();
     res.status(200).send(folders);
+  } catch (error) {
+    next(error);
+  }
+});
+
+foldersRouter.get("/:id", async (req, res, next) => {
+  try {
+    const id = Number(req.params.id);
+
+    const folders = await getFolders();
+    const matchingFolder = folders.find((folder) => folder.id === id);
+
+    if (!matchingFolder) {
+      return res.status(404).send(`Folder with id ${id} does not exist.`);
+    }
+
+    const folder = await getFolderById(id);
+    res.status(200).send(folder);
   } catch (error) {
     next(error);
   }
--- db/queries/folders.js
+++ db/queries/folders.js
@@ -23,3 +23,20 @@ export async function getFolders() {
 
   return rows;
 }
+
+// json_agg(files) is a function that takes all the rows from the files table that match the condition, and turn them into a JSON array
+export async function getFolderById(id) {
+  const sql = `
+    SELECT folders.*,
+    (
+      SELECT json_agg(files)
+      FROM files
+      WHERE files.folder_id = folders.id
+    ) AS files
+    FROM folders
+    WHERE folders.id = $1;
+  `;
+  const { rows } = await db.query(sql, [id]);
+
+  return rows[0];
+}
